ccc9719212b033710768cdab786e99e98688eee1
Carlos <chspv2007@gmail.com>
2025-12-02 17:40:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,21 +19,17 @@ def calculadora():
         else:
             print('Algo deu errado, digite apenas números inteiros.')
 
-
 def soma(a=float, b=float):
     soma = a + b
     return soma
-
 
 def subtracao(a=float, b=float):
     subtracao = a - b
     return subtracao
 
-
 def multiplicacao(a=float, b=float):
     multiplicacao = a * b
     return multiplicacao
-
 
 def divisao(a=float, b=float):
     if b != 0:
