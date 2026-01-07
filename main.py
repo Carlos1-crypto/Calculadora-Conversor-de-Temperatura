a3d8bfd26bd2bb023f05254281ccf3d7a6ba2ccf
Carlos <chspv2007@gmail.com>
2026-01-07 09:43:48 -0300

    Otimização de if's/elif's e else
    Substituição de if/elif e else por match e case

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def calculadora():
               "4. Divisão\n"
               "5. Sair\n")
         operacao = (input("Escolha a operação: "))
+
         try:
             operacao = int(operacao)
         except ValueError:
@@ -39,29 +40,30 @@ def divisao(a=float, b=float):
         print("Divisão por zero não é permitida.")
 
 def em_funcionamento(operacao):
-    if operacao == 1:
-        escolha = 'soma'
-    elif operacao == 2:
-        escolha = 'subtração'
-    elif operacao == 3:
-        escolha == 'multiplicação'
-    elif operacao == 4:
-        escolha = 'divisão'
-    elif operacao == 5:
-        print('Entendido. Antes de ir, você gostaria de usar o conversor de temperatura? (S/N)')
-        resp = input('Resposta: ')
-        if resp.isdigit():
-            print('Algo deu errado, digite apenas sim ou não.')
-            em_funcionamento(operacao=5)
-        elif resp.isalpha():
-            if resp in ['s', 'sim']:
-                conversor_temperatura()
-            elif resp in ['n', 'não', 'nao']:
-                print('Ok, saindo...')
-                exit()
-            else:
+    match operacao:
+        case 1:
+            escolha = 'soma'
+        case 2:
+            escolha = 'subtração'
+        case 3:
+            escolha == 'multiplicação'
+        case 4:
+            escolha = 'divisão'
+        case 5:
+            print('Entendido. Antes de ir, você gostaria de usar o conversor de temperatura? (S/N)')
+            resp = input('Resposta: ')
+            if resp.isdigit():
                 print('Algo deu errado, digite apenas sim ou não.')
                 em_funcionamento(operacao=5)
+            elif resp.isalpha():
+                if resp in ['s', 'sim']:
+                    conversor_temperatura()
+                elif resp in ['n', 'não', 'nao']:
+                    print('Ok, saindo...')
+                    exit()
+                else:
+                    print('Algo deu errado, digite apenas sim ou não.')
+                    em_funcionamento(operacao=5)
 
     print(f"Você escolheu a operação {escolha}.\n"
           "Digite os números:")
@@ -70,26 +72,22 @@ def em_funcionamento(operacao):
     try:
         num1 = float(num1)
         num2 = float(num2)
-
     except ValueError:
         print('Algo deu errado, digite apenas números.')
         return em_funcionamento(operacao)
 
-    if operacao == 1:
-        print(f'Resultado: {soma(num1, num2)}\n')
-
-    elif operacao == 2:
-        print(f'Resultado: {subtracao(num1, num2)}\n')
-
-    elif operacao == 3:
-        print(f'Resultado: {multiplicacao(num1, num2)}\n')
-
-    elif operacao == 4:
-        print(f'Resultado: {divisao(num1, num2)}\n')
-
-print("====Bem Vindo ao Conversor de Temperatura!====\n")
-
+    match operacao:
+        case 1:
+            print(f'Resultado: {soma(num1, num2)}\n')
+        case 2:
+            print(f'Resultado: {subtracao(num1, num2)}\n')
+        case 3:
+            print(f'Resultado: {multiplicacao(num1, num2)}\n')
+        case 4:
+            print(f'Resultado: {divisao(num1, num2)}\n')
+    
 def conversor_temperatura():
+    print("====Bem Vindo ao Conversor de Temperatura!====\n")
     while True:
         print("1. Celsius para Fahrenheit\n"
               "2. Fahrenheit para Celsius\n"
@@ -102,16 +100,17 @@ def conversor_temperatura():
             print('Algo deu errado, digite apenas números.')
             return conversor_temperatura()
 
-        if escolha == 1:
-            C_F()
-        elif escolha == 2:
-            F_C()
-        elif escolha == 3:
-            print('Saindo...')
-            exit()
-        else:
-            print("Opção inválida. Tente novamente.")
-            conversor_temperatura()
+        match escolha:
+            case 1:
+                C_F()
+            case 2:
+                F_C()
+            case 3:
+                print('Saindo...')
+                exit()
+            case _:
+                print("Opção inválida. Tente novamente.")
+                conversor_temperatura()
 
 def C_F():
     graus = (input("Digite a temperatura em Celsius: "))
